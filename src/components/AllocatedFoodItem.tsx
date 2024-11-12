2fbc88d7f40fe73a8023b6944606ff9177439f41
tom-gordon13 <tombcgordon@gmail.com>
2024-11-11 20:24:21 -0700

    Give user ability to move item side to side with keys

--- src/components/AllocatedFoodItem.tsx
+++ src/components/AllocatedFoodItem.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Box } from '@mui/material';
 import { AllocatedItem } from '../interfaces/AllocatedItem';
 
@@ -8,19 +8,45 @@ interface FoodItemContainerProps {
 }
 
 export const AllocatedFoodItem: React.FC<FoodItemContainerProps> = ({ foodItem }) => {
+    const [isInEditMode, setIsInEditMode] = useState<boolean>(false);
+    const [position, setPosition] = useState({ x: foodItem.x, y: foodItem.y });
+
+    const handleClick = () => {
+        setIsInEditMode(true)
+    }
+
+    const handleKeyDown = (e: KeyboardEvent) => {
+        if (!isInEditMode) return
+
+        if (e.key === 'ArrowRight') setPosition((prev) => ({ ...prev, x: prev.x + 3 }));
+        if (e.key === 'ArrowLeft') setPosition((prev) => ({ ...prev, x: prev.x - 3 }));
+    }
+
+    useEffect(() => {
+        if (isInEditMode) {
+            window.addEventListener('keydown', handleKeyDown);
+        } else {
+            window.removeEventListener('keydown', handleKeyDown);
+        }
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [isInEditMode])
 
     return (
         <Box
             sx={{
                 position: 'absolute',
-                top: foodItem.y,
-                left: foodItem.x,
+                top: position.y,
+                left: position.x,
                 padding: '0.5rem',
                 backgroundColor: 'lightgreen',
                 border: '1px solid black'
             }}
+            onClick={handleClick}
         >
-            {foodItem.id} - {foodItem.x}, {foodItem.y}
+            {foodItem.id} - {position.x}, {position.y}
         </Box>
     );
 }
