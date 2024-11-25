759e56c6e7d035d2a3a9020b8553c5e48c4ba38d
tom-gordon13 <tombcgordon@gmail.com>
2024-11-25 08:02:30 -0500

    Add food search panel

--- ui/src/pages/Home.tsx
+++ ui/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box, Grid } from '@mui/material';
+import { Box, Grid, Drawer, Button } from '@mui/material';
 import { StagingContainer } from '../components/StagingContainer';
 import { RaceContainerTop } from '../components/RaceContainerTop'
 import { NavMain } from '../components/NavMain';
@@ -12,12 +12,21 @@ const raceDurationSample = 10
 
 export const Home = () => {
     const { allocatedItems, setAllocatedItems } = useAllocatedItems();
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
     fetchItem('skratch')
+
+    const toggleDrawer = (newOpen: boolean) => () => {
+        setIsDrawerOpen(newOpen);
+    };
 
     return (
         <>
             <NavMain />
             <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'start', alignItems: 'center' }}>
+                <Button onClick={toggleDrawer(true)}>Search for Items</Button>
+                <Drawer open={isDrawerOpen} onClose={toggleDrawer(false)} anchor='right'>
+                    Food search panel
+                </Drawer>
 
                 <Grid container spacing={2} alignItems="flex-start">
                     <Grid item xs={8}>
@@ -27,7 +36,9 @@ export const Home = () => {
                         <NutritionInfoContainer />
                     </Grid>
                 </Grid>
+
                 <RaceContainerTop raceDuration={raceDurationSample} />
+
             </Box>
         </>
     );
