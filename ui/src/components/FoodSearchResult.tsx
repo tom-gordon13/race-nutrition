fdc65d298ff626963c1631a03a9cf3ca00611ff4
tom-gordon13 <tombcgordon@gmail.com>
2024-11-27 15:45:25 -0500

    Add tooltip on hover to give additional food information

--- ui/src/components/FoodSearchResult.tsx
+++ ui/src/components/FoodSearchResult.tsx
@@ -3,7 +3,7 @@ import { dropTargetForElements, monitorForElements, draggable } from '@atlaskit/
 import { attachClosestEdge } from "@atlaskit/pragmatic-drag-and-drop-hitbox/closest-edge";
 import { combine } from "@atlaskit/pragmatic-drag-and-drop/combine";
 import invariant from 'tiny-invariant';
-import { Box } from '@mui/material';
+import { Box, Tooltip } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 
 
@@ -19,24 +19,38 @@ const containerDimensions = {
 export const FoodSearchResult: React.FC<FoodSearchResult> = ({ item }) => {
     const theme = useTheme();
 
+
     return (
-        <Box
-            sx={{
-                position: 'absolute',
-                height: containerDimensions.height,
-                width: containerDimensions.width,
-                padding: '0.5rem',
-                bgcolor: theme.palette.primary.main,
-                '&:hover': {
-                    bgcolor: theme.palette.primary.light
-                },
-                borderRadius: 2,
-                boxShadow: 3,
-                color: 'white',
-                fontWeight: 'bold',
-            }}
-        >
-            {item.brandName} - {item.description}
-        </Box>
+        <Tooltip
+            title={
+                <>
+                    Brand Name: {item.brandName}
+                    <br />
+                    Brand Owner: {item.brandOwner}
+                    <br />
+                    Description: {item.description}
+                    <br />
+                    Category: {item.foodCategory}
+                </>
+            } arrow>
+            <Box
+                sx={{
+                    position: 'absolute',
+                    height: containerDimensions.height,
+                    width: containerDimensions.width,
+                    padding: '0.5rem',
+                    bgcolor: theme.palette.primary.main,
+                    '&:hover': {
+                        bgcolor: theme.palette.primary.light
+                    },
+                    borderRadius: 2,
+                    boxShadow: 3,
+                    color: 'white',
+                    fontWeight: 'bold',
+                }}
+            >
+                {item.brandName} - {item.description}
+            </Box>
+        </Tooltip>
     );
 }
