19ad0c2830e214873cfdf64f93c144da91558347
tom-gordon13 <tombcgordon@gmail.com>
2024-12-06 14:54:15 -0700

    Styling and add clear button to food search page

--- ui/src/components/FoodSearchContainer.tsx
+++ ui/src/components/FoodSearchContainer.tsx
@@ -16,6 +16,9 @@ export const FoodSearchContainer: React.FC<FoodSearchContainerProps> = ({ addToS
     const [pageError, setPageError] = useState<boolean>(false)
     const [loading, setLoading] = useState<boolean>(false);
 
+    const handleClear = () => {
+        setSearchResults([])
+    }
 
     const handleSearch = () => {
         const fetchData = async () => {
@@ -42,48 +45,64 @@ export const FoodSearchContainer: React.FC<FoodSearchContainerProps> = ({ addToS
 
     return (
         <Box sx={{
-            width: '100%'
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            paddingTop: '1rem'
         }}>
-            Search for an item: {'   '}
-            <Input
-                value={inputValue}
-                onChange={(e) => setInputValue(e.target.value)}
-                onKeyDown={handleKeyDown}
-                placeholder="Enter item"
-            />{'   '}
-            <Button variant='contained' onClick={handleSearch}>Search</Button>
-            <br />
-            {loading && (
-                <Box
-                    sx={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        marginTop: '1rem',
-                    }}
-                >
-                    <CircularProgress />
-                </Box>
-            )}
-            {pageError && !loading && (
-                <Box
-                    sx={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        height: '20rem',
-                    }}
-                >
-                    <h3>Unable to fetch results</h3>
-                </Box>
-            )}
+            <Box
+                sx={{
+                    width: '100%',
+                    display: 'flex',
+                    flexDirection: 'row',
+                    gap: '5px',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    paddingTop: '1rem'
+                }}>
+                Search for an item: {'   '}
+                <Input
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    onKeyDown={handleKeyDown}
+                    placeholder="Enter item"
+                />{'   '}
+                <Button variant='contained' onClick={handleSearch}>Search</Button>
+                <Button variant='outlined' onClick={handleClear}>Clear Items</Button>
+                <br />
+                {loading && (
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            marginTop: '1rem',
+                        }}
+                    >
+                        <CircularProgress />
+                    </Box>
+                )}
+                {pageError && !loading && (
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            height: '20rem',
+                        }}
+                    >
+                        <h3>Unable to fetch results</h3>
+                    </Box>
+                )}
+            </Box>
             {!pageError && !loading && (
                 <Grid
                     container
                     columnSpacing={3}
                     rowSpacing={12}
-                    sx={{ display: 'flex', alignItems: 'center', gap: '10px', flexWrap: 'wrap', margin: 0 }}
+                    sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '10px', flexWrap: 'wrap', margin: 0 }}
                 >
                     {searchResults.map((item, index) => (
                         <Grid item xs={2.5} key={index}>
